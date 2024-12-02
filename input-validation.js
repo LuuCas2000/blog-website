e6af5af7148ca5d366209410087833afe0944fe1
Lucas Souza <LucasRSouza3500@gmail.com>
2024-12-02 18:55:44 -0300

    building the register/login/new article UI page + input sanitizer

---
+++ input-validation.js
@@ -0,0 +1,18 @@
+import { validationResult } from 'express-validator';
+
+const validateInput = validations => {
+    return async (req, res, next) => {
+        // sequential processing, stops running validations chain if one fails.
+        for (const validation of validations) {
+            const result = await validation.run(req);
+            if (!result.isEmpty()) {
+                return res.status(400).json({ errors: result.array() });
+                //throw new Error(result.array())
+            }
+        }
+
+        next();
+    }
+};
+
+export default validateInput;
